1530e758f2255e919d663d5d583f0af5ac256935
cjm7433 <cjm7433@psu.edu>
2026-01-20 22:39:47 -0500

    update formatting and style

--- src/Audio/audio.ads
+++ src/Audio/audio.ads
@@ -17,17 +17,18 @@ package Audio is
    ) return IC.int;
 
    pragma Import (Stdcall, mciSendStringA, "mciSendStringA");
-   
-   procedure Play_Audio(File_Path : String);
+
+   procedure Play_Audio (File_Path : String);
    procedure Cleanup_Sounds;
 
-   private
-      use Ada.Strings.Unbounded;
+private
+   use Ada.Strings.Unbounded;
 
-      package Unbounded_Strings renames Ada.Strings.Unbounded;
-      package Alias_Vectors is new Ada.Containers.Vectors (Index_Type => Natural, Element_Type => Unbounded_Strings.Unbounded_String);
+   package Unbounded_Strings renames Ada.Strings.Unbounded;
+   package Alias_Vectors is new Ada.Containers.Vectors (Index_Type =>
+      Natural, Element_Type => Unbounded_Strings.Unbounded_String);
 
-      Tracked_Aliases   : Alias_Vectors.Vector;
-      Audio_ID          : Natural := 0;
+   Tracked_Aliases   : Alias_Vectors.Vector;
+   Audio_ID          : Natural := 0;
 
 end Audio;
