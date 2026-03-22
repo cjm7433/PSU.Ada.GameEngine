c6449d11c5ff35dba58c225a559c01a6306534d1
cjm7433 <cjm7433@psu.edu>
2026-03-22 16:06:59 -0400

    fixing merge conflicts

--- src/Windows/wave_player.ads
+++ src/Windows/wave_player.ads
@@ -11,8 +11,4 @@ private
    pragma Linker_Options ("-lole32");
    pragma Linker_Options ("-lxaudio2_9");
 
-<<<<<<< HEAD:src/Windows/wave_player.ads
 end Wave_Player;
-=======
-end Wave_Player;
->>>>>>> main:src/Audio/wave_player.ads
